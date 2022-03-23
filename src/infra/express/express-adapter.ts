e3f423b87886f37428d77c6549fbe49335ef7b9d
ArianNexux <ariannexux0101@gmail.com>
2022-03-23 14:37:31 +0100

    feat: create my express adapter

---
+++ src/infra/express/express-adapter.ts
@@ -0,0 +1,16 @@
+import Controller from "../../presentation/controllers/contracts/controller";
+import { HttpRequest, HttpResponse } from "../../presentation/controllers/contracts/http";
+import { Request, Response } from "express";
+
+export const expressAdapter = (controller: Controller) => {
+    return async (req: Request, res: Response) =>{
+        const httpRequest : HttpRequest = {
+            body: req.body,
+            params: req.params
+        }
+
+        const response : HttpResponse = await  controller.handle(httpRequest)
+        
+        res.status(response.statusCode).json(response.body)
+    }
+}

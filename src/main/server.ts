aa885b4bf200aaa5a7cfb03b3c3e45d338ce7736
ArianNexux <ariannexux0101@gmail.com>
2022-03-23 16:38:52 +0100

    refactor: separete routes of article in a single

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,27 +1,17 @@
 import { connect } from 'mongoose'
 import 'dotenv/config'
-import express from  'express'
-import { expressAdapter } from '../infra/express/express-adapter'
-import { FactoryListArticleController } from './factories/factory-list-article'
-import { FactoryCreateArticleController } from './factories/factory-create-article'
-import { FactoryListArticleByIdController } from './factories/factory-list-article-by-id'
-import { FactoryUpdateArticleController } from './factories/factory-update-article'
-import { FactoryDeleteArticleController } from './factories/factory-delete-article'
+import express from 'express'
+import { ArticlesRoutes } from './articles.routes'
 
-const app =  express()
+
+const app = express()
 
 app.use(express.json())
-
-app.get('/articles/', expressAdapter(FactoryListArticleController()))
-app.post('/articles/', expressAdapter(FactoryCreateArticleController()))
-app.get('/articles/:id', expressAdapter(FactoryListArticleByIdController()))
-app.put('/articles/:id', expressAdapter(FactoryUpdateArticleController()))
-app.delete('/articles/:id', expressAdapter(FactoryDeleteArticleController()))
-
-connect(`mongodb+srv://coodesh:${process.env.DB_PASSWORD}@cluster0.twujv.mongodb.net/myFirstDatabase`).then(()=>{
-    app.listen(3000, function(){
+app.use(ArticlesRoutes)
+connect(`mongodb+srv://coodesh:${process.env.DB_PASSWORD}@cluster0.twujv.mongodb.net/myFirstDatabase`).then(() => {
+    app.listen(3000, function () {
         console.log("Running on 3000 Port")
     })
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 })

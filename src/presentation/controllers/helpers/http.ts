f35eab6b5536d8d542012cc47d739a4d19e1a516
ArianNexux <ariannexux0101@gmail.com>
2022-03-23 15:15:02 +0100

    fix: change status code

--- src/presentation/controllers/helpers/http.ts
+++ src/presentation/controllers/helpers/http.ts
@@ -1,16 +1,16 @@
 import { HttpResponse } from "../contracts/http";
 import { MissingParam } from "../errors/missing-param";
 
-export const ok = (result: any) : HttpResponse => {
+export const ok = (result: any): HttpResponse => {
     return {
         body: result,
         statusCode: 200
     }
 }
 
-export const unprocessableEntity = (param) : HttpResponse =>{
+export const unprocessableEntity = (param): HttpResponse => {
     return {
         body: new MissingParam(param),
-        statusCode: 400
+        statusCode: 422
     }
 }

fc9c166a8befddf92d500229cf57402a63a6afd5
ArianNexux <ariannexux0101@gmail.com>
2022-03-23 16:09:51 +0100

    test:make sure tha return 422 if no featured is provided

--- src/tests/create-article.spec.ts
+++ src/tests/create-article.spec.ts
@@ -92,6 +92,23 @@ describe('Create Article', () => {
         const httpResponse: HttpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(422)
     })
+
+    test('should return 422 if no summary is featured', async () => {
+        const sut = FactoryCreateArticleController()
+        const httpRequest: HttpRequest = {
+            body: {
+                id: 1,
+                summary: "teste",
+                title: "teste",
+                url: "teste",
+                imageUrl: "teste",
+                newsSite: "teste",
+                publishedAt: "2021-05-13T00:48:12.000Z"
+            }
+        }
+        const httpResponse: HttpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(422)
+    })
     afterAll(async () => {
         await connection.close()
     })

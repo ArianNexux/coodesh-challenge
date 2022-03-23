211369dd8194df1acba8b94b77d33b960526c62d
ArianNexux <ariannexux0101@gmail.com>
2022-03-23 15:14:33 +0100

    feat: add validation  of fields

--- src/presentation/controllers/create-article.ts
+++ src/presentation/controllers/create-article.ts
@@ -1,17 +1,25 @@
 import IService from "../services/contracts/IService";
 import Controller from "./contracts/controller";
 import { HttpRequest, HttpResponse } from "./contracts/http";
-import { ok } from "./helpers/http";
+import { ok, unprocessableEntity } from "./helpers/http";
 
 
-export default class CreateArticleController implements Controller{
-    constructor(private readonly service: IService){
+export default class CreateArticleController implements Controller {
+    constructor(private readonly service: IService) {
 
     }
-    
+
     async handle(request: HttpRequest): Promise<HttpResponse> {
+        const requiredFields = ['title'];
+        for (const field in requiredFields) {
+            if (!request.body[field]) {
+                return unprocessableEntity(field)
+            }
+        }
+
         const res = await this.service.handle(request.body)
+
         return ok(res);
     }
-    
+
 }

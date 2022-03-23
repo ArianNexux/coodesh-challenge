09e255e8f80559c5d0dfd5d1ae859b6fbb5b30c6
ArianNexux <ariannexux0101@gmail.com>
2022-03-23 14:34:00 +0100

    feat: create my helpers for http protocol

---
+++ src/presentation/controllers/helpers/http.ts
@@ -0,0 +1,16 @@
+import { HttpResponse } from "../contracts/http";
+import { MissingParam } from "../errors/missing-param";
+
+export const ok = (result: any) : HttpResponse => {
+    return {
+        body: result,
+        statusCode: 200
+    }
+}
+
+export const unprocessableEntity = (param) : HttpResponse =>{
+    return {
+        body: new MissingParam(param),
+        statusCode: 400
+    }
+}

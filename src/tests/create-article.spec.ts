3ab205f9eac6a56b482286fa33964af1a9d7259b
ArianNexux <ariannexux0101@gmail.com>
2022-03-23 15:15:42 +0100

    test: make sure that return 422 if no title is provided

--- src/tests/create-article.spec.ts
+++ src/tests/create-article.spec.ts
@@ -1,21 +1,30 @@
 import { FactoryCreateArticleController } from "../main/factories/factory-create-article"
+import { connect, connection } from 'mongoose'
+import 'dotenv/config'
 import { HttpRequest, HttpResponse } from '../presentation/controllers/contracts/http'
-describe('Create Article', () => { 
 
-    test('should return 422 if no title is provided', async () => { 
+
+describe('Create Article', () => {
+    beforeAll(async () => {
+        await connect(`mongodb+srv://coodesh:${process.env.DB_PASSWORD}@cluster0.twujv.mongodb.net/myFirstDatabase`)
+    })
+    test('should return 422 if no title is provided', async () => {
         const sut = FactoryCreateArticleController()
         const httpRequest: HttpRequest = {
             body: {
                 id: 1,
                 featured: true,
                 url: "teste",
-                imageUrl:"teste",
+                imageUrl: "teste",
                 newsSite: "teste",
-                summary:"teste",
+                summary: "teste",
                 publishedAt: "2021-05-13T00:48:12.000Z"
             }
         }
         const httpResponse: HttpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(422)
-     })
- })
+    })
+    afterAll(async () => {
+        await connection.close()
+    })
+})

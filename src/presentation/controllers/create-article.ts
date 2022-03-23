01229e84f28e8dda210c7c230166c0d7ccf85d7f
ArianNexux <ariannexux0101@gmail.com>
2022-03-23 16:12:54 +0100

    feat: return 422 if no featured is provided

--- src/presentation/controllers/create-article.ts
+++ src/presentation/controllers/create-article.ts
@@ -10,7 +10,7 @@ export default class CreateArticleController implements Controller {
     }
 
     async handle(request: HttpRequest): Promise<HttpResponse> {
-        const requiredFields = ["title", "url", "imageUrl", "newsSite", "summary"];
+        const requiredFields = ["title", "url", "imageUrl", "newsSite", "summary", "featured"];
         for (let index = 0; index < requiredFields.length; index++) {
             if (!request.body[requiredFields[index]])
                 return unprocessableEntity(requiredFields[index])

1f915e7a9761a23c9272fe65d98b7d60599f2865
ArianNexux <ariannexux0101@gmail.com>
2022-03-24 10:49:05 +0100

    test: create test integration

---
+++ src/tests/articles.route.spec.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import { connect, connection } from 'mongoose'
+import { app } from '../main/server'
+import { HttpResponse } from '../presentation/controllers/contracts/http'
+describe('Test routes of articles', () => {
+    beforeAll(async () => {
+        await connect(`mongodb+srv://coodesh:${process.env.DB_PASSWORD}@cluster0.twujv.mongodb.net/myFirstDatabase`)
+    })
+
+    test('should create an article', async () => {
+        const res: HttpResponse = await request(app)
+            .post("/articles/")
+            .send({
+                id: 1,
+                featured: true,
+                title: "teste",
+                url: "teste",
+                imageUrl: "teste",
+                newsSite: "teste",
+                summary: "teste",
+                publishedAt: "2021-05-13T00:48:12.000Z"
+            }).set('Content-Type', 'application\/json')
+        expect(res.statusCode).toBe(200)
+    })
+
+    afterAll(async () => {
+        await connection.close()
+    })
+})

7b7de1201a1ac810199b8edbf6d43d24eedd1441
ArianNexux <ariannexux0101@gmail.com>
2022-03-24 10:45:34 +0100

    fix: append middlw to

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,10 +3,10 @@ import 'dotenv/config'
 import express from 'express'
 import { ArticlesRoutes } from './articles.routes'
 
-
-const app = express()
+export const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(ArticlesRoutes)
 connect(`mongodb+srv://coodesh:${process.env.DB_PASSWORD}@cluster0.twujv.mongodb.net/myFirstDatabase`).then(() => {
     app.listen(3000, function () {

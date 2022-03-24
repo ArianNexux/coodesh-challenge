86e181a99892e07f0d88e74273a704be1fca3f33
ArianNexux <ariannexux0101@gmail.com>
2022-03-24 10:44:05 +0100

    fix: return right object

--- src/infra/express/express-adapter.ts
+++ src/infra/express/express-adapter.ts
@@ -3,14 +3,14 @@ import { HttpRequest, HttpResponse } from "../../presentation/controllers/contra
 import { Request, Response } from "express";
 
 export const expressAdapter = (controller: Controller) => {
-    return async (req: Request, res: Response) =>{
-        const httpRequest : HttpRequest = {
+    return async (req: Request, res: Response) => {
+        const httpRequest: HttpRequest = {
             body: req.body,
             params: req.params
         }
 
-        const response : HttpResponse = await  controller.handle(httpRequest)
-        
+        const response: HttpResponse = await controller.handle(httpRequest)
+
         res.status(response.statusCode).json(response.body)
     }
 }

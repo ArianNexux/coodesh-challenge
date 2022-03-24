0b6cd514152e786f8e9ee5482187e4c7d00a4ec9
ArianNexux <ariannexux0101@gmail.com>
2022-03-24 10:46:11 +0100

    fix: append middlware of urlencoded

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,7 +4,6 @@ import express from 'express'
 import { ArticlesRoutes } from './articles.routes'
 
 export const app = express()
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(ArticlesRoutes)

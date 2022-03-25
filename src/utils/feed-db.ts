194b839f40ac72f69e0e1bb162743c4db5d3f033
ArianNexux <ariannexux0101@gmail.com>
2022-03-25 17:09:26 +0100

    feat: create script to insert data on database

---
+++ src/utils/feed-db.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+(async function () {
+    try {
+        const { status, data } = await axios.get('https://api.spaceflightnewsapi.net/v3/articles')
+        data.map(async (param) => {
+            const body = {
+                ...param
+            }
+            const res = await axios.post('http://localhost:3000/articles/', body)
+            console.log(res.status)
+        })
+    } catch (err) {
+        console.log(err.getMessage())
+    }
+
+})()
+

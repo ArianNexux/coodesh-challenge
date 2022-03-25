011c3f5e908043df3857480373042d08eaf61fb6
ArianNexux <ariannexux0101@gmail.com>
2022-03-25 18:33:40 +0100

    feat: start cron-job after server initialize

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,8 @@
 import { connect } from 'mongoose'
 import 'dotenv/config'
 import express from 'express'
-import { ArticlesRoutes } from './articles.routes'
-
+import { ArticlesRoutes } from './express/articles.routes'
+import { job } from '../utils/feed-db'
 export const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -10,7 +10,9 @@ app.use(ArticlesRoutes)
 connect(`mongodb+srv://coodesh:${process.env.DB_PASSWORD}@cluster0.twujv.mongodb.net/myFirstDatabase`).then(() => {
     app.listen(3000, function () {
         console.log("Running on 3000 Port")
+        job.start();
     })
+
 }).catch((err) => {
     console.log(err)
 })

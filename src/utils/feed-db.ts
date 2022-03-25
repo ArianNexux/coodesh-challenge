dc91b3d7ff6748dc95b97be7467b421d345601b9
ArianNexux <ariannexux0101@gmail.com>
2022-03-25 18:17:27 +0100

    refactor: delete console.log of debugs

--- src/utils/feed-db.ts
+++ src/utils/feed-db.ts
@@ -4,9 +4,7 @@ import axios from "axios";
     try {
         const { status, data } = await axios.get('https://api.spaceflightnewsapi.net/v3/articles')
         data.map(async (param) => {
-
             const res = await axios.post('http://localhost:3000/articles/', ...param)
-            console.log(res.status)
         })
     } catch (err) {
         console.log(err.getMessage())

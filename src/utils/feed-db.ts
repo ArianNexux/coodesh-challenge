c4fc9a4e4affe06209177813df9620d5aa27434b
ArianNexux <ariannexux0101@gmail.com>
2022-03-25 18:18:20 +0100

    refactor: map only if status request is 200

--- src/utils/feed-db.ts
+++ src/utils/feed-db.ts
@@ -3,9 +3,10 @@ import axios from "axios";
 (async function () {
     try {
         const { status, data } = await axios.get('https://api.spaceflightnewsapi.net/v3/articles')
-        data.map(async (param) => {
-            const res = await axios.post('http://localhost:3000/articles/', ...param)
-        })
+        if (status == 200)
+            data.map(async (param) => {
+                const res = await axios.post('http://localhost:3000/articles/', ...param)
+            })
     } catch (err) {
         console.log(err.getMessage())
     }

ad6724b6c9ff8c770086b53a52c4fe9236a51f30
ArianNexux <ariannexux0101@gmail.com>
2022-04-28 18:56:35 +0100

    refactor: implement pagination

--- src/infra/mongodb/list-article.ts
+++ src/infra/mongodb/list-article.ts
@@ -1,11 +1,14 @@
 import { Article } from "../../core/entities/article";
-import { IListArticleRepository } from "../contracts/ilist-article-repository";
+import { IListArticleRepository, IPagination } from "../contracts/ilist-article-repository";
 import { articleModel } from './schemas/article'
-export class ListArticleMongoDb implements IListArticleRepository{
-    
-    async listArticle(): Promise<Article[]> {
-        const res = await articleModel.find({})
+export class ListArticleMongoDb implements IListArticleRepository {
+
+    async listArticle({ pageNumber, registPerPage }: IPagination): Promise<Article[]> {
+        const res = await articleModel.find()
+            .sort({ _id: 1 })
+            .skip(pageNumber > 0 ? ((pageNumber - 1) * registPerPage) : 0)
+            .limit(registPerPage)
         return res
     }
-    
+
 }
